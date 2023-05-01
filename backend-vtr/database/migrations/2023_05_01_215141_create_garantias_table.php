17c7f65b11dd3e72708d4a6f52953fdc270cd382
Gustavo da Silva Freitas <gustavo-silvafreitas@hotmail.com>
2023-05-01 19:09:28 -0300

    Commit inicial criando estrutura do banco de dados

---
+++ backend-vtr/database/migrations/2023_05_01_215141_create_garantias_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('garantias', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('usuario_id')->references('id')->on('usuarios');
+            $table->foreign('produto_id')->references('id')->on('produtos');
+            $table->string('hash', 32)->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('garantias');
+    }
+};
---
+++ backend-vtr/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // \App\Models\Usuario::factory(10)->create();
+
+        // \App\Models\Usuario::factory()->create([
+        //     'name' => 'Test Usuario',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}

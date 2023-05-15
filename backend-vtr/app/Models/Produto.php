e82f0160b80ec106efe0bd2d149784bd23a6df18
Gustavo da Silva Freitas <gustavo-silvafreitas@hotmail.com>
2023-05-14 22:53:02 -0300

    rota de listagem de produtos funcional

--- backend-vtr/app/Models/Produto.php
+++ backend-vtr/app/Models/Produto.php
@@ -4,8 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Produto extends Model
 {
     use HasFactory;
+
+    static function listagemProduto(){
+        $response = DB::table('produtos')
+                    ->select('produtos.nome', 'produtos.descricao', 'imagem_produtos.caminho')
+                    ->join('imagem_produtos', 'produtos.id', '=', 'imagem_produtos.produto_id')
+                    ->get();
+        return $response;
+    }
 }

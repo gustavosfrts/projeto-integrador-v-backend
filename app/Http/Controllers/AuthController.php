276d213435def98499b06ca6e35eb61b962130e2
Tetelof <dannieltetelof@ucl.br>
2023-05-22 22:24:54 +0000

    Devolvendo dados do usuario no login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,41 +3,67 @@
 namespace App\Http\Controllers;
 
 use App\Models\Usuario;
+use Exception;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     public function login(Request $request)
     {
-        $credentials = $request->only('email', 'password');
-        $token = auth('api')->attempt($credentials);
+        try {
+            $credentials = $request->only('email', 'password');
+            $token = auth('api')->attempt($credentials);
 
-        if (!$token) {
-            return response()->json(['error' => 'Senha ou email inválido.'], 500);
+            if (!$token) {
+                return response()->json([
+                    'error' => 'Senha ou email inválido.'
+                ], 500);
+            }
+
+            $user = Usuario::where('email', $request->email)->first();
+
+            $user['token'] = $token;
+            return response()->json($user, 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'error' => 'Erro: ' . $e->getMessage()
+            ], 500);
         }
-
-        return response()->json(['token' => $token]);
     }
 
     function register(Request $request)
     {
-        $data = $request->only('name', 'email', 'cpfcnpj', 'telefone', 'password');
+        try {
+            $data = $request->only(
+                'name',
+                'email',
+                'cpfcnpj',
+                'telefone',
+                'password'
+            );
 
-        $user = Usuario::create([
-            'nome' => $data['name'],
-            'email' => $data['email'],
-            'cpfcnpj' => $data['cpfcnpj'],
-            'telefone' => $data['telefone'],
-            'password' => Hash::make($data['password']),
-        ]);
+            $user = Usuario::create([
+                'nome' => $data['name'],
+                'email' => $data['email'],
+                'cpfcnpj' => $data['cpfcnpj'],
+                'telefone' => $data['telefone'],
+                'password' => Hash::make($data['password']),
+            ]);
+            $user->save();
 
-        $token = auth('api')->attempt([$data['email'], $data['password']]);
+            $token = auth('api')->attempt(
+                [$data['email'], $data['password']]
+            );
 
-        return response()->json([
-            'user' => $user,
-            'token' => $token
-        ], 201);
+            return response()->json([
+                'user' => $user,
+                'token' => $token
+            ], 201);
+        } catch (Exception $e) {
+            return response()->json([
+                'error' => 'Erro: ' . $e->getMessage()
+            ], 500);
+        }
     }
 }

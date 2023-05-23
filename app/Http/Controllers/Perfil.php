cfd3842389dff2cee14b7e3d2bb48950649de28d
Tetelof <dannieltetelof@ucl.br>
2023-05-23 00:24:59 +0000

    rota de updare do perfil

--- app/Http/Controllers/Perfil.php
+++ app/Http/Controllers/Perfil.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Exception;
 use Illuminate\Http\Request;
 use \App\Models\ImagemPerfil;
+use App\Models\Usuario;
 
 class Perfil extends Controller
 {
@@ -12,7 +13,21 @@ class Perfil extends Controller
     {
         try {
             $id = $request->user()->attributesToArray()['id'];
-            $imagemPerfil = ImagemPerfil::getImagemPerfil($id);
+            $response = Usuario::getUsuario($id);
+            $response['imagem_perfil'] = ImagemPerfil::getImagemPerfil($id);
+
+            return response()->json($response, 200);
+        } catch (Exception $e) {
+            return response()->json([
+                'error' => 'Erro: ' . $e->getMessage()
+            ], 500);
+        }
+    }
+
+    public function update(Request $request) {
+        try {
+            $id = $request->user()->attributesToArray()['id'];
+            $imagemPerfil = ImagemPerfil::updateImagemPerfil($id, $request->imagem);
 
             return response()->json($imagemPerfil, 200);
         } catch (Exception $e) {

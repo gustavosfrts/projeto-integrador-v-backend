b53aa1d391284796b599475cf0a26abbf48a9a3a
Tetelof <dannieltetelof@ucl.br>
2023-06-23 23:39:37 +0000

    Rotas de forums publicas

--- routes/api.php
+++ routes/api.php
@@ -30,12 +30,15 @@ Route::group(['middleware' => ['jwt.auth']], function () {
     Route::prefix('auth/produtos')->group(function () {
         Route::get('/', [Produto::class, 'listagemProdutosLogado'])->name('api.produtos.listagem');
     });
+
     Route::prefix('garantias')->group(function () {
         Route::get('/', [Garantia::class, 'listagemGarantias'])->name('api.garantias.listagem');
     });
+
     Route::prefix('manuais')->group(function () {
         Route::get('/', [Manual::class, 'listagemManuais'])->name('api.manuais.listagem');
     });
+
     Route::prefix('perfis')->group(function () {
         Route::get('/', [Perfil::class, 'perfil'])->name('api.perfil');
         Route::put('/', [Perfil::class, 'update'])->name('api.perfil.update');
@@ -45,20 +48,7 @@ Route::group(['middleware' => ['jwt.auth']], function () {
     Route::prefix('transferencia')->group(function () {
         Route::post('/', [Produto::class, 'transferencia'])->name('api.tranferencia');
     });
-    Route::prefix('forums')->group(function () {
-        Route::get('/', [Forum::class, 'listagemForums'])->name('api.forums.listagem');
-        Route::get('/{id}', [Forum::class, 'forum'])->name('api.forum');
-        Route::post('/', [Forum::class, 'createForum'])->name('api.forum.create');
-        Route::put('/{id}', [Forum::class, 'updateForum'])->name('api.forum.update');
-        Route::delete('/{id}', [Forum::class, 'deleteForum'])->name('api.forum.delete');
-        Route::prefix('comentarios')->group(function () {
-            Route::post('/', [Forum::class, 'createComentario'])->name('api.comentarios.create');
-            Route::put('/{id}', [Forum::class, 'updateComentario'])->name('api.comentarios.update');
-            Route::delete('/{id}', [Forum::class, 'deleteComentario'])->where('id', '[0-9]+')->name('api.comentarios.delete');
-            Route::post('/like', [Forum::class, 'likeComentario'])->name('api.comentarios.like');
-            Route::delete('/unlike', [Forum::class, 'unlikeComentario'])->name('api.comentarios.unlike');
-        });
-    });
+
     Route::prefix('images')->group(function () {
         Route::post('upload', [ImageController::class, 'storeImage'])->name('api.images.upload');
     });
@@ -71,7 +61,6 @@ Route::group(['middleware' => ['jwt.auth']], function () {
 
 Route::prefix('notificacao')->group(function () {
     Route::post('/enviar', [NotificationController::class, 'enviarNotificacao'])->name('api.notificacao.enviar');
-
 });
 
 Route::prefix('produtos')->group(function () {
@@ -80,3 +69,21 @@ Route::prefix('produtos')->group(function () {
 
 Route::post("/login", [AuthController::class, "login"]);
 Route::post("/register", [AuthController::class, "register"]);
+
+Route::prefix('forums')->group(function () {
+    Route::get('/', [Forum::class, 'listagemForums'])->name('api.forums.listagem');
+    Route::get('/{id}', [Forum::class, 'forum'])->name('api.forum');
+
+    Route::group(['middleware' => ['jwt.auth']], function () {
+        Route::post('/', [Forum::class, 'createForum'])->name('api.forum.create');
+        Route::put('/{id}', [Forum::class, 'updateForum'])->name('api.forum.update');
+        Route::delete('/{id}', [Forum::class, 'deleteForum'])->name('api.forum.delete');
+        Route::prefix('comentarios')->group(function () {
+            Route::post('/', [Forum::class, 'createComentario'])->name('api.comentarios.create');
+            Route::put('/{id}', [Forum::class, 'updateComentario'])->name('api.comentarios.update');
+            Route::delete('/{id}', [Forum::class, 'deleteComentario'])->where('id', '[0-9]+')->name('api.comentarios.delete');
+            Route::post('/like', [Forum::class, 'likeComentario'])->name('api.comentarios.like');
+            Route::delete('/unlike', [Forum::class, 'unlikeComentario'])->name('api.comentarios.unlike');
+        });
+    });
+});
